3fa313fe6b026daf2a81666c3f090b552eb3e717
jose_muse <87675826+Mjzo@users.noreply.github.com>
2022-10-13 16:18:36 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,9 @@
-i
+#!/usr/bin/python3
+"""File defines a square"""
+
+
+class Square:
+    """A class Square that defines a square"""
+    def __init__(self, size=None):
+        """Initializes private attribute size """
+        self.__size = size
