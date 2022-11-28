24cee1920204a28f16c423a4d91cdaac7cc3bbd9
jose_muse <87675826+Mjzo@users.noreply.github.com>
2022-11-29 01:23:29 +0300

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,12 @@
-i
+#!/usr/bin/python3
+"""
+================================
+module with method is_same_class
+================================
+"""
+
+
+def is_same_class(obj, a_class):
+    """Method that return True if an object is an instance of a class"""
+
+    return type(obj) is a_class
