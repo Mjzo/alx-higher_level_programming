7671b37a7be0c50ebf2d4f2e3976becc6f6def71
jose_muse <87675826+Mjzo@users.noreply.github.com>
2022-11-29 01:47:38 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,13 @@
-i
+#!/usr/bin/python3
+"""
+===================================
+module with method inherits_from
+===================================
+"""
+
+
+def inherits_from(obj, a_class):
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
+
+    return False if type(obj) is a_class else isinstance(obj, a_class)
